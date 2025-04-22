3f04dd62f583a890e34c09b37b0fce51eef4ed79
Raimundas <rm.rimkus@gmail.com>
2025-04-22 17:08:20 +0000

    feat: add project

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
+# Create a new project
+resource "digitalocean_project" "bka_k8s" {
+  name        = "bka-k8s"
+  description = "Project for BKA Kubernetes cluster"
+  purpose     = "Web Application Hosting"
+  environment = "Development"
+}
+
+# Your VPC
 data "digitalocean_vpc" "fra1" {
   name = "default-fra1"
 }
 
+# Kubernetes cluster
 resource "digitalocean_kubernetes_cluster" "fra1_single_node" {
-  name    = "fra1-single-node"
+  name    = "do-bka-single-node-cluster"
   region  = "fra1"
   version = "1.32.2-do.0"
 
@@ -14,5 +24,13 @@ resource "digitalocean_kubernetes_cluster" "fra1_single_node" {
   }
 
   vpc_uuid = data.digitalocean_vpc.fra1.id
+}
+
+# Assign the cluster to the new project
+resource "digitalocean_project_resources" "assign_cluster_to_project" {
+  project = digitalocean_project.bka_k8s.id
+  resources = [
+    digitalocean_kubernetes_cluster.fra1_single_node.urn
+  ]
 }
 
